c7a62d9d7623f145fd5f453f59fb27aeaab524f8
atsushi130 <atsushi_main@outlook.jp>
2018-07-28 17:55:51 +0900

    implement: Script

--- Sources/Scripty/Scripty.swift
+++ Sources/Scripty/Scripty.swift
@@ -1,3 +1,20 @@
-struct Scripty {
-    var text = "Hello, World!"
+
+import Foundation
+
+public struct Script {
+    
+    fileprivate let script: String
+    
+    public init(_ script: String) {
+        self.script = script
+    }
+    
+    public func exec() {
+        Process.launchedProcess(launchPath: "/bin/sh", arguments: ["-c", self.script]).waitUntilExit()
+    }
+}
+
+infix operator |
+public func | (lhs: Script, rhs: String) -> Script {
+    return Script(lhs.script + "|" + rhs)
 }
